a5ddab1497b0311b61539bd5c8568400e80efb70
saivandanavukku <saivandanavukku@gmail.com>
2021-03-20 13:12:54 +0530

    Completed

---
+++ book-app-master/book-app-master/favouriteAPI/favouriteAPI/Repository/IFavouriteRepo.cs
@@ -0,0 +1,16 @@
+﻿using favouriteAPI.Models;
+using System.Collections.Generic;
+
+namespace favouriteAPI.Repository
+{
+    public interface IFavouriteRepo
+    {
+        List<Favourite> GetFavourites();
+        List<Favourite> GetFavourite(string userId);
+        Favourite AddFavourite(Favourite favourite);
+        bool DeleteFavourite(string userId, string Title);
+        public bool IsFavouriteExistWithId(string userId);
+        public bool IsFavouriteExist(Favourite favourite);
+        object DeleteFavourite(string id);
+    }
+}
